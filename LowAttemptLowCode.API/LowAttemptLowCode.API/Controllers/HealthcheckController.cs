ad249f443f257eff402cea60f7d1738e7a3b1ef1
Austin Kispotta <austinkispotta@gmail.com>
2022-11-08 15:10:50 +0530

    Change baseURL

---
+++ LowAttemptLowCode.API/LowAttemptLowCode.API/Controllers/HealthcheckController.cs
@@ -0,0 +1,18 @@
+﻿using LowAttemptLowCode.API.Entities.Request;
+using LowAttemptLowCode.API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LowAttemptLowCode.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthcheckController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<string> Healthcheck()
+        {
+            return "Hello from Healthcheck controller!";
+        }
+    }
+}
--- LowAttemptLowCode.UI/src/environments/environment.prod.ts
+++ LowAttemptLowCode.UI/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://api-lowattemptlowcode.pcfreakz.co.in',
-  angularBaseUrl: 'https://lowattemptlowcode.pcfreakz.co.in'
+  baseUrl: 'https://lowattemptlowcodeapi.azurewebsites.net',
+  angularBaseUrl: 'https://www.pcfreakz.co.in'
 };
